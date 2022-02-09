fd31d1478a732c36bd4687d5d3d8babefd64b318
Gaurav <gaurav@Khushis-MacBook-Pro.local>
2022-02-09 08:42:10 +0545

    initial commit

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int count;
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        count = 10;
+        Log.d(TAG, "count:"+String.valueOf(count));
+    }
+}
